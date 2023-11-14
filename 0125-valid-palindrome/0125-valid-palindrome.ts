e6dc405d0b74bf77ac43c5b01c02b8b62388b315
TinTin <86914748+TinTinWinata@users.noreply.github.com>
2023-11-14 22:46:34 +0700

    Time: 285 ms (5.02%), Space: 61.5 MB (5.17%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -0,0 +1,31 @@
+const notValid = `~\`!@#$%^&*()_+-=[{]}\\|'";:/?.>,<"' `
+const isValid = (val: string) => !notValid.includes(val) 
+
+function isPalindrome(s: string): boolean {
+    s = s.toLowerCase();
+    let pivot1 = 0;
+    let pivot2 = s.length -1;
+    while(true){
+        console.log(`Pivot1[${pivot1}] (${s[pivot1]}) and Pivot2[${pivot2}] (${s[pivot2]})`)
+        if(pivot2 <= pivot1){
+            return true;
+        }
+        if(!isValid(s[pivot1])){
+            pivot1++;
+            continue;
+        }
+        if(!isValid(s[pivot2])){
+            pivot2--;
+            continue;
+        }
+        
+//      All 2 valid
+        if(s[pivot1] === s[pivot2]){
+            pivot1++;
+            pivot2--;
+        }else{
+            return false;
+        }
+    }
+    return false;
+};
