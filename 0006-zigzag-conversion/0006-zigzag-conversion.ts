972e17b33a8e8d69fbd7d977d22a4d00f4de37d9
TinTin <86914748+TinTinWinata@users.noreply.github.com>
2023-11-15 22:09:00 +0700

    Time: 842 ms (5.05%), Space: 78.6 MB (5.05%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.ts
@@ -0,0 +1,35 @@
+function convert(s: string, numRows: number): string {
+    if(s.length === 1 || numRows === 1 || s.length < numRows) return s
+    const maxRange = Math.ceil(s.length / numRows);
+    const space = numRows - 2;
+    const xMax = maxRange + (maxRange - 1) * space + 1;
+    const yMax = numRows;
+    const arr = new Array(xMax).fill(null).map(() => new Array(yMax).fill(''))
+    let p = 0;
+    for (let k=0;k<maxRange;k++){
+        for(let i=0;i<numRows;i++){
+            if(p >= s.length) break;
+            const x = k * (numRows - 1);
+            const y= i;
+            arr[x][y]=s.charAt(p);
+            p += 1;
+        }
+        for(let i=0;i<space;i++){
+            if(p >= s.length) break;
+            const x = ((numRows - 1) * k) + 1 + i;
+            const y = numRows - 2 - i;
+            arr[x][y] = s.charAt(p)
+            p += 1;
+        }
+        if(p >= s.length) break;
+     
+    }
+    let result = ''
+    console.log(arr)
+    for(let i=0;i<yMax;i++){
+        for(let j=0;j<xMax;j++){
+            if(arr[j][i] !== '') result += arr[j][i]
+        }
+    }
+    return result
+};
