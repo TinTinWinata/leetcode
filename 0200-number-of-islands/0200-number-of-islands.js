6d0688ec7d9931a4122f005637331dc101cb8f6b
TinTin <86914748+TinTinWinata@users.noreply.github.com>
2023-11-20 15:48:04 +0700

    Time: 74 ms (54.36%), Space: 43.7 MB (99.88%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,31 +3,34 @@
  * @return {number}
  */
 
-const recrusive = (x, y, arr, grid, xLen, yLen) => {
+const recrusive = (x, y, grid, xLen, yLen) => {
     
-    if( x < 0 || y < 0 || x >= xLen || y >= yLen || arr[y][x] === 1 || grid[y][x] === "0"){
+    if( x < 0 || y < 0 || x >= xLen || y >= yLen || grid[y][x] === "2" || grid[y][x] === "0"){
         return;
     }
-    arr[y][x] = 1;
-    recrusive(x + 1, y, arr, grid, xLen, yLen);
-    recrusive(x - 1, y, arr, grid, xLen, yLen);
-    recrusive(x, y + 1, arr, grid, xLen, yLen);
-    recrusive(x, y - 1, arr, grid, xLen, yLen);
+    grid[y][x] = "2";
+    recrusive(x + 1, y, grid, xLen, yLen);
+    recrusive(x - 1, y, grid, xLen, yLen);
+    recrusive(x, y + 1, grid, xLen, yLen);
+    recrusive(x, y - 1, grid, xLen, yLen);
 }
 
 var numIslands = function(grid) {
+//     0 -> water
+//     1 -> land
+//     2 -> visited
     const yLen = grid.length;
     const xLen = grid[0].length; 
     let arr = new Array(yLen).fill(0).map(() => new Array(xLen).fill(0))
     let res = 0;
     for(let y=0;y<yLen;y++){
         for(let x=0;x<xLen;x++){
-            if(arr[y][x] === 1){ // check already visited
+            if(grid[y][x] === "2"){ // check already visited
                 continue;
             }
             if(grid[y][x] === "1"){
                 res += 1;
-                recrusive(x, y, arr, grid, xLen, yLen);
+                recrusive(x, y,  grid, xLen, yLen);
             }
         }
     }
