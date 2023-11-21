b252756f66c71f727db0a0b9beac7272b1c4bed2
TinTin <86914748+TinTinWinata@users.noreply.github.com>
2023-11-21 13:58:51 +0700

    Time: 5566 ms (5.02%), Space: 184.6 MB (5.07%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+
+
+const recrusive = (first, curr, map) => {
+    // console.log(`first ${first} | curr ${curr} | map `, map , ` | curr map | `, map[curr])
+
+    if(map[curr] === undefined){
+        // console.log('return true')
+        return true;
+    }
+    if(first.includes(curr)){
+        // console.log(`return on ${curr} : `, first)
+        return false;
+    }
+    first.push(curr);
+    for(const val of map[curr]){
+        if(!recrusive([...first], val, map)) 
+        {
+            return false;
+        }
+
+    }
+    return true;
+}
+
+var canFinish = function(numCourses, prerequisites) {
+    const map = {}
+    for(let i=0;i<prerequisites.length;i++){
+        const value = prerequisites[i][1];
+        const key = prerequisites[i][0];
+        if(map[key]){
+            map[key].push(value);
+        }else{
+            map[key] = [value];
+        }
+    }
+    console.log(`map : `, map)
+    for(const [key, value] in map){
+        console.log('Checking on ', key, value)
+        if(!recrusive([], key, map)) return false;
+    }
+    return true;
+};
