4525f0b29db9afca198bf9def64bc663beec1c66
TinTin <86914748+TinTinWinata@users.noreply.github.com>
2023-11-21 18:42:53 +0700

    Time: 55 ms (33.85%), Space: 41.9 MB (45.62%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,7 +3,5 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    return (BigInt(digits.reduce(function(total, value){
-        return total + value;
-    },"")) + 1n).toString().split('')
+    return (BigInt(digits.reduce(function(total, value){return total + value;},"")) + 1n).toString().split('')
 };
