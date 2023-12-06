293bb91b2793eaf00aed79a1f59e955e4ee8f132
TinTin <86914748+TinTinWinata@users.noreply.github.com>
2023-12-06 16:39:40 +0700

    Time: 63 ms (22.86%), Space: 42.7 MB (69.97%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.ts
+++ 0392-is-subsequence/0392-is-subsequence.ts
@@ -1,12 +1,16 @@
 function isSubsequence(s: string, t: string): boolean {
     let j = 0;
+    if(s === ""){
+        return true
+    }
     for(let i = 0; i < t.length; i++){
         if (t.charAt(i) == s.charAt(j)){
             j++;
+            if(j == s.length){
+                return true
+            }
         }
     }
-    if(j == s.length){
-        return true
-    }
+
     return false
 };
