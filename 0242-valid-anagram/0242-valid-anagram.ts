1c75cb61a881313672bd01ff47bd4330d83d1d2d
TinTin <86914748+TinTinWinata@users.noreply.github.com>
2023-12-16 08:01:04 +0700

    Time: 66 ms (84.79%), Space: 45.1 MB (67.05%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -0,0 +1,31 @@
+
+function isAnagram(s: string, t: string): boolean {
+    const map = new Map();
+    if(t.length !== s.length){
+        return false;
+    }
+    
+    for(const v of s){
+        if(map.has(v)){
+            let n = map.get(v)
+            n = n + 1;
+            map.set(v, n)
+        }else{
+            map.set(v, 1)
+        }
+    }
+    // console.log('map : ', map)
+    for(const v of t){
+        if(map.has(v)){
+            let n = map.get(v);
+            n = n - 1;
+            map.set(v, n);
+            if(n < 0){
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+    return true;
+};
